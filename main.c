3240c7193450d7731c8817691ee13aa4010c3281
Yan103 <101476599+Yan103@users.noreply.github.com>
2024-08-17 23:08:27 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,36 +2,71 @@
 #include <math.h>
 #include <windows.h>
 
-#define EPS 0.00000001
+const double EPS = 1e-8;
 
-/* Алгоритм решения*/
-void solution(double *a, double *b, double *c) {
-    if (fabs(*a) < EPS) {
-        if (fabs(*b) < EPS) {
-            if (fabs(*c) < EPS) { printf("Бесконечно много решений\n"); }
-            else { printf("Решений нет\n"); }
-        }
-        else { printf("1 решение: %.3f\n", (fabs(*c) < EPS) ? 0 : - *c / *b);}
-    }
-    else {
-        double D = *b * *b - 4 * *a * *c;
+/* Информация об уравнении */
+struct equation {
+    const double a, b, c; /* Коэффициенты */
+    const double D;       /* Дискриминант */
+    double x1, x2;  /* Корни х1 < x2 (если корень один - он записан в х1) */
+    char solve_cnt; /* Количество решений: '0', '1', '2' - очевидно, 'A' - бесконечно много (any) */
+};
 
-        if (D < EPS && D > -EPS) {
-            printf("1 решение: %.3f\n", (fabs(*b) < EPS) ? 0 : - *b / (2 * *a));
+/* Инициализация уравнения */
+struct equation make_equation(const double *a, const double *b, const double *c) {
+    const struct equation q_equation = {*a, *b, *c, *b * *b - 4 * *a * *c, 0, 0, '3'}; /* 0 и '3' по умолчанию, чтобы отлавливать ошибки */
+    return q_equation;
+}
+
+/* Решение уравнения (получаем информацию о количестве корней и сами корни) */
+static void solve_equation(struct equation *equation) {
+    if (fabs(equation->a) < EPS) {
+        if (fabs(equation->b) < EPS) {
+            if (fabs(equation->c) < EPS) {
+                equation->solve_cnt = 'A';
+            } else {
+                equation->solve_cnt = '0';
+            }
+        } else {
+            equation->solve_cnt = '1';
+            equation->x1 = (fabs(equation->c) < EPS) ? 0 : - equation->c / equation->b;
         }
-        else if (D < -EPS) { printf("Нет действительных корней\n"); } /* Нужно ли искать комплексные корни? */
-        else {
-            printf("2 решения: %.3f и %.3f\n", (- *b - sqrt(D)) / (2 * *a), (- *b + sqrt(D)) / (2 * *a));
+    } else {
+        if (equation->D < EPS && equation->D > -EPS) {
+            equation->solve_cnt = '1';
+            equation->x1 = (fabs(equation->b) < EPS) ? 0 : - equation->b / (2 * equation->a);
+        } else if (equation->D < -EPS) {
+            equation->solve_cnt = '0';
+        } else {
+            equation->solve_cnt = '2';
+            equation->x1 = (- equation->b - sqrt(equation->D)) / (2 * equation->a);
+            equation->x2 = (- equation->b + sqrt(equation->D)) / (2 * equation->a);
         }
     }
 }
 
+/* Вывод результата */
+static void show_result(const struct equation *equation) {
+    if (equation->solve_cnt == '0') {
+        printf("Уравнение не имеет действительных корней\n");
+    } else if (equation->solve_cnt == '1') {
+        printf("Уравнение имеет 1 корень: %.3f\n", equation->x1);
+    } else if (equation->solve_cnt == '2') {
+        printf("Уравнение имеет 2 корня: %.3f и %.3f\n", equation->x1, equation->x2);
+    } else {
+        printf("Уравнение имеет бесконечное количество решений\n");
+    }
+}
+
 int main() {
-    SetConsoleOutputCP(CP_UTF8); /* Без этого Clion не выводит кириллицу :( */
-    double a, b, c;
-    printf("Введите коэффицент при a:"), scanf("%lf", &a);
-    printf("Введите коэффицент при b:"), scanf("%lf", &b);
-    printf("Введите коэффицент при c:"), scanf("%lf", &c);
-    solution(&a, &b, &c);
+    SetConsoleOutputCP(CP_UTF8); /* Без этого Clion не выводит кириллицу:(*/
+    double a = 0, b = 0, c = 0;
+    printf("Введите через пробел коэффициенты при a, b и c:");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    struct equation quadratic_equation = make_equation(&a, &b, &c);
+    struct equation *pp = &quadratic_equation;
+    solve_equation(pp);
+    show_result(pp);
     return 0;
 }

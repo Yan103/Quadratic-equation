ac8177265080d806e01f88faf4747210217a8b5d
Yan103 <101476599+Yan103@users.noreply.github.com>
2024-08-18 11:37:02 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -19,40 +19,40 @@ struct equation make_equation(const double *a, const double *b, const double *c)
 }
 
 /* Решение уравнения (получаем информацию о количестве корней и сами корни) */
-static void solve_equation(struct equation *equation) {
-    if (fabs(equation->a) < EPS) {
-        if (fabs(equation->b) < EPS) {
-            if (fabs(equation->c) < EPS) {
-                equation->solve_cnt = 'A';
+static void solve_equation(struct equation *q_equation) {
+    if (fabs(q_equation->a) < EPS) {
+        if (fabs(q_equation->b) < EPS) {
+            if (fabs(q_equation->c) < EPS) {
+                q_equation->solve_cnt = 'A';
             } else {
-                equation->solve_cnt = '0';
+                q_equation->solve_cnt = '0';
             }
         } else {
-            equation->solve_cnt = '1';
-            equation->x1 = (fabs(equation->c) < EPS) ? 0 : - equation->c / equation->b;
+            q_equation->solve_cnt = '1';
+            q_equation->x1 = (fabs(q_equation->c) < EPS) ? 0 : - q_equation->c / q_equation->b;
         }
     } else {
-        if (equation->D < EPS && equation->D > -EPS) {
-            equation->solve_cnt = '1';
-            equation->x1 = (fabs(equation->b) < EPS) ? 0 : - equation->b / (2 * equation->a);
-        } else if (equation->D < -EPS) {
-            equation->solve_cnt = '0';
+        if (q_equation->D < EPS && q_equation->D > -EPS) {
+            q_equation->solve_cnt = '1';
+            q_equation->x1 = (fabs(q_equation->b) < EPS) ? 0 : - q_equation->b / (2 * q_equation->a);
+        } else if (q_equation->D < -EPS) {
+            q_equation->solve_cnt = '0';
         } else {
-            equation->solve_cnt = '2';
-            equation->x1 = (- equation->b - sqrt(equation->D)) / (2 * equation->a);
-            equation->x2 = (- equation->b + sqrt(equation->D)) / (2 * equation->a);
+            q_equation->solve_cnt = '2';
+            q_equation->x1 = (- q_equation->b - sqrt(q_equation->D)) / (2 * q_equation->a);
+            q_equation->x2 = (- q_equation->b + sqrt(q_equation->D)) / (2 * q_equation->a);
         }
     }
 }
 
 /* Вывод результата */
-static void show_result(const struct equation *equation) {
-    if (equation->solve_cnt == '0') {
+static void show_result(const struct equation *q_equation) {
+    if (q_equation->solve_cnt == '0') {
         printf("Уравнение не имеет действительных корней\n");
-    } else if (equation->solve_cnt == '1') {
-        printf("Уравнение имеет 1 корень: %.3f\n", equation->x1);
-    } else if (equation->solve_cnt == '2') {
-        printf("Уравнение имеет 2 корня: %.3f и %.3f\n", equation->x1, equation->x2);
+    } else if (q_equation->solve_cnt == '1') {
+        printf("Уравнение имеет 1 корень: %.3f\n", q_equation->x1);
+    } else if (q_equation->solve_cnt == '2') {
+        printf("Уравнение имеет 2 корня: %.3f и %.3f\n", q_equation->x1, q_equation->x2);
     } else {
         printf("Уравнение имеет бесконечное количество решений\n");
     }
@@ -63,10 +63,8 @@ int main() {
     double a = 0, b = 0, c = 0;
     printf("Введите через пробел коэффициенты при a, b и c:");
     scanf("%lf %lf %lf", &a, &b, &c);
-
     struct equation quadratic_equation = make_equation(&a, &b, &c);
-    struct equation *pp = &quadratic_equation;
-    solve_equation(pp);
-    show_result(pp);
+    solve_equation(&quadratic_equation);
+    show_result(&quadratic_equation);
     return 0;
 }
